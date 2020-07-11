ffd2bb4e828a588e17bf8e8c8eb7b4113ed5888b
adeel rajwani <adeel.rajwani@utexas.edu>
2020-07-11 10:46:49 -0500

    adding simple login button

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import Pads from './Drumpad.js';
+import Login from './Login'
 
 
 function App(props) {
   return (
-    <div className='drum' id='drum-machine'>
-      <Pads />
-      <div className='footer' id='drum-footer'>
-        Made by Adeel Rajwani & Keilen Wiley
+    <div id="root">
+      <Login />
+      <div className='drum' id='drum-machine'>
+        <Pads />
+        <div className='footer' id='drum-footer'>
+          Made by Adeel Rajwani & Keilen Wiley
+      </div>
       </div>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Login() {
+  return (
+    <div>
+      <button className="login" id="login">Login</button>
+    </div>
+  )
+}
+
+
+export default Login;
